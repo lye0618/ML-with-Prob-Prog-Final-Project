e3873b60e206fe967520c9af22cffbca241ecde2
Serena Liu <zl2729@columbia.edu>
2019-11-13 01:00:28 -0500

    Update proprocessing.py

--- proprocessing.py
+++ proprocessing.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[41]:
-
-
 import os
 from functools import partial
 import numpy as np
@@ -38,8 +32,9 @@ norm_by_asset_factor = ['Capital Expenditures','Goodwill','Fixed Assets']
 data[norm_by_asset_factor] = data[norm_by_asset_factor].div(data['Total Assets'], axis=0)
 data['FCF Margin']= data['Net Cash Flow-Operating']/data['Total Revenue']
 
-data.dropna(subset=['return'],inplace=True)#, 'born'])
+data.dropna(subset=['return','symbol'],inplace=True)#, 'born'])
 
+data=data.fillna(0)
 
 features = ['e2p', 'Cash Ratio','Gross Margin','Profit Margin','Current Ratio','Operating Margin','Short-Term Debt / Current Portion of Long-Term Debt','FCF Margin']+norm_by_asset_factor
 
@@ -50,65 +45,11 @@ y_train = data['return'].values
 
 
 
-stats.zscore(X_train, axis=1, ddof=1)
+X_train=stats.zscore(X_train, axis=1, ddof=1)
+y_train=stats.zscore(y_train, axis=1, ddof=1)
+
 
 X_data = torch.tensor(X_train, dtype=torch.float)
 y_data = torch.tensor(y_train, dtype=torch.float)
-
-
-
-
-# In[47]:
-
-
-from scipy import stats
-stats.zscore(X_train, axis=1, ddof=1)
-
-
-# In[51]:
-
-
-
-
-
-# In[45]:
-
-
-len(price.date.unique())
-
-
-# In[19]:
-
-
-len(data)
-
-
-# In[43]:
-
-
-X_data.size()
-
-
-# In[26]:
-
-
-data[norm_by_asset_factor]
-
-
-# In[29]:
-
-
-data['Gross Margin']
-
-
-# In[31]:
-
-
-data['Gross Profit']/data['Total Revenue']
-
-
-# In[ ]:
-
-
 
 
